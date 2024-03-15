b15a9f782da2e55393916796e3f72e74a5e0faad
Shahidul Islam Majumder <dev@shahid.pro>
2024-03-15 11:30:31 +0600

    feat: refactor prettier config

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,67 +1,70 @@
-import { formatDistanceToNow } from "date-fns";
-import CommentActions from "./CommentActions";
-import UserProfile from "./UserProfile";
-import CommentVotes from "./CommentVotes";
+import { formatDistanceToNow } from 'date-fns';
+import CommentActions from './CommentActions';
+import UserProfile from './UserProfile';
+import CommentVotes from './CommentVotes';
 
 export interface Comment {
-  id: number;
-  created_by: number;
-  body: string;
-  created_at: string;
-  updated_at: string;
-  votes: number;
+    id: number;
+    created_by: number;
+    body: string;
+    created_at: string;
+    updated_at: string;
+    votes: number;
 }
 
 export interface CommentData extends Comment {
-  replies?: Comment[];
+    replies?: Comment[];
 }
 
 export interface User {
-  id: number;
-  gender: string;
-  name: { title: string; first: string; last: string };
-  email: string;
-  phone: string;
-  cell: string;
-  picture: {
-    large: string;
-    medium: string;
-    thumbnail: string;
-  };
-  nat: string;
+    id: number;
+    gender: string;
+    name: { title: string; first: string; last: string };
+    email: string;
+    phone: string;
+    cell: string;
+    picture: {
+        large: string;
+        medium: string;
+        thumbnail: string;
+    };
+    nat: string;
 }
 
 export type CommentProps = {
-  comment: CommentData;
-  user: User;
-  currentUser?: User;
+    comment: CommentData;
+    user: User;
+    currentUser?: User;
 };
 
 function Comment({ comment, user, currentUser }: CommentProps) {
-  return (
-    <div className="flex bg-white p-6 rounded-lg mb-6 last:mb-0">
-      <CommentVotes currentVotes={comment.votes} />
-      <div className="pl-6">
-        <div className="flex justify-between">
-          <div className="flex items-center  *:mr-4 last:*:mr-0">
-            <UserProfile user={user} currentUser={currentUser} />
-            <p className="text-gray-700/70 font-normal">
-              {`${formatDistanceToNow(new Date(comment.created_at))}`.replace(
-                "about ",
-                "",
-              )}{" "}
-              ago
-            </p>
-          </div>
-          <CommentActions currentUser={currentUser} userId={user.id} />
+    return (
+        <div className="flex bg-white p-6 rounded-lg mb-6 last:mb-0">
+            <CommentVotes currentVotes={comment.votes} />
+            <div className="pl-6">
+                <div className="flex justify-between">
+                    <div className="flex items-center  *:mr-4 last:*:mr-0">
+                        <UserProfile user={user} currentUser={currentUser} />
+                        <p className="text-gray-700/70 font-normal">
+                            {`${formatDistanceToNow(new Date(comment.created_at))}`.replace(
+                                'about ',
+                                ''
+                            )}{' '}
+                            ago
+                        </p>
+                    </div>
+                    <CommentActions
+                        currentUser={currentUser}
+                        userId={user.id}
+                    />
+                </div>
+                <p
+                    className="pt-4 text-gray-700/70 [&>a]:font-bold"
+                    dangerouslySetInnerHTML={{ __html: comment.body }}
+                ></p>
+            </div>
         </div>
-        <p
-          className="pt-4 text-gray-700/70 [&>a]:font-bold"
-          dangerouslySetInnerHTML={{ __html: comment.body }}
-        ></p>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Comment;
--- src/components/CommentBox.tsx
+++ src/components/CommentBox.tsx
@@ -1,29 +1,29 @@
-import { User } from "./Comment";
+import { User } from './Comment';
 
 interface CommentBoxProps {
-  user: User;
+    user: User;
 }
 
 function CommentBox({ user }: CommentBoxProps) {
-  return (
-    <div className="w-full mt-6 flex *:mr-4 last:*:mr-0 bg-white p-6 rounded-md">
-      <div className="w-14 h-14">
-        <img
-          src={user.picture.thumbnail}
-          alt="johndoe"
-          className="w-full h-auto rounded-full"
-        />
-      </div>
-      <textarea
-        className="w-full border py-2 px-6 rounded-md resize-none placeholder:text-gray-700/80"
-        placeholder="Add a comment..."
-        rows={3}
-      />
-      <button className="text-white uppercase bg-indigo-700 py-3 h-full px-8 rounded-md ">
-        Send
-      </button>
-    </div>
-  );
+    return (
+        <div className="w-full mt-6 flex *:mr-4 last:*:mr-0 bg-white p-6 rounded-md">
+            <div className="w-14 h-14">
+                <img
+                    src={user.picture.thumbnail}
+                    alt="johndoe"
+                    className="w-full h-auto rounded-full"
+                />
+            </div>
+            <textarea
+                className="w-full border py-2 px-6 rounded-md resize-none placeholder:text-gray-700/80"
+                placeholder="Add a comment..."
+                rows={3}
+            />
+            <button className="text-white uppercase bg-indigo-700 py-3 h-full px-8 rounded-md ">
+                Send
+            </button>
+        </div>
+    );
 }
 
 export default CommentBox;
