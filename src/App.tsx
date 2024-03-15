1d321144b0206506af316c02f52f861dbd3e4afc
Shahidul Islam Majumder <dev@shahid.pro>
2024-03-15 11:29:53 +0600

    feat: refactor prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
-import CommentBox from "./components/CommentBox";
-import CommentList from "./components/CommentList";
-import comments from "./data/comments.json";
-import users from "./data/users.json";
-import { User } from "./components/Comment";
+import { useState } from 'react';
+import CommentBox from './components/CommentBox';
+import CommentList from './components/CommentList';
+import comments from './data/comments.json';
+import users from './data/users.json';
+import { User } from './components/Comment';
 
 function App() {
-  const [currentUser, _setCurrentUser] = useState<User>(users[1]);
+    const [currentUser, _setCurrentUser] = useState<User>(users[1]);
 
-  return (
-    <div className="max-w-3xl px-4 mx-auto my-16">
-      <CommentList
-        comments={comments}
-        users={users}
-        currentUser={currentUser}
-      />
-      <CommentBox user={currentUser} />
-    </div>
-  );
+    return (
+        <div className="max-w-3xl px-4 mx-auto my-16">
+            <CommentList
+                comments={comments}
+                users={users}
+                currentUser={currentUser}
+            />
+            <CommentBox user={currentUser} />
+        </div>
+    );
 }
 
 export default App;
